51f9955bcaafa6aec55906efd23bc87231ac9fda
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-19 12:14:49 -0700

    add questions and jquery conditional update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// var C# = 0
+// var Ruby = 0
+// var PHP= 0
+// var CSS/Design = 0
+
+
+
 $(document).ready(function() {
   $('button').click(function(){
     if(this.id =='begin'){
@@ -10,5 +17,14 @@ $(document).ready(function() {
       $('#brain').show();
     }
 
+    if(this.id == 'go_on'){
+      $('#brain').hide();
+      $('#work').show();
+    }
+
+    if(this.id == 'next1'){
+      $(".work").hide();
+      // $('#work').show();
+    }
 });
 });
