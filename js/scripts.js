5b7d711444940b4ca9d851d91cc10ad98febf54c
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-19 13:55:29 -0700

    build script logic ***HTML tags need fix

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,55 @@ var p=0;
 var j=0;
 
 function countAnswers(qq1, qq2, qq3, qq4,){
-  if (q1 == "yes") {
-    ww++;
-    jb++;
+// question 1 logic
+  if (q1 == "1") {
+    j++;
+    p++;
+  } else if (q1 == "2") {
+    c++;
+    r++;
+  } else if (q1 == "3") {
+    c++;
+    r++;
+    j++;
+    p++;
+  }
+// question 2 logic
+  if (q2 == "1") {
+    p++;
+    r++;
+  } else if (q2 == "2") {
+    c++;
+    p++;
+    j++;
+  } else if (q2 == "3") {
+    c++;
+  } else if (q2 == "4") {
+    j++;
+    r++;
+  }
+// question 3 logic
+  if (q3 =="yes") {
+    r++;
+    p++;
   } else {
-    hp++;
-    dt++;
-
+    c++;
+    j++;
+    p++;
+  }
+// question 4 logic Below
+  if (q4 == "a") {
+    j++;
+    r++;
+  } else if (q4 == "b") {
+    c++;
+    p++;
+  } else if (q4 == "c") {
+    r++;
+    j++;
+    p++;
+  } else if (q4 == "d") {
+    j++;
   }
 }
 
@@ -18,13 +60,13 @@ $(document).ready(function() {
 $('#formOne').submit(function(event) {
   event.preventDefault();
   var name = $("#nme").val();
-  console.log(name);
   var qq1 = $('#q1').val();
   var qq2 = $('#q2').val();
   var qq3 = $('#q3').val();
   var qq4 = $('#q4').val();
 })
 
+console.log(j);
   // code for next buttons
   $('button').click(function(){
 
