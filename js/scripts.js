8f39e52f3a40db157130528b063589fe3528ef52
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-19 16:41:23 -0700

    attempt to fix broken code. Divs no longer fade

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,63 @@
-var c=0;
-var r=0;
-var p=0;
-var j=0;
-
-console.log(j);
-function countAnswers(qq1, qq2, qq3, qq4,){
-// question 1 logic
-  if (qq1 == "1") {
-    j++;
-    p++;
-  } else if (qq1 == "2") {
-    c++;
-    r++;
-  } else if (qq1 == "3") {
-    c++;
-    r++;
-    j++;
-    p++;
-  }
-// question 2 logic
-  if (qq2 == "1") {
-    p++;
-    r++;
-  } else if (qq2 == "2") {
-    c++;
-    p++;
-    j++;
-  } else if (qq2 == "3") {
-    c++;
-  } else if (qq2 == "4") {
-    j++;
-    r++;
-  }
-// question 3 logic
-  if (qq3 =="yes") {
-    r++;
-    p++;
-  } else {
-    c++;
-    j++;
-    p++;
-  }
-// question 4 logic Below
-  if (qq4 == "a") {
-    j++;
-    r++;
-  } else if (qq4 == "b") {
-    c++;
-    p++;
-  } else if (qq4 == "c") {
-    r++;
-    j++;
-    p++;
-  } else if (qq4 == "d") {
-    j++;
-  }
-}
+// var c=0;
+// var r=0;
+// var p=0;
+// var j=0;
+//
+//
+// function countAnswers(qq1, qq2, qq3, qq4,){
+// // question 1 logic
+//   if (qq1 == "1") {
+//     j++;
+//     p++;
+//   } else if (qq1 == "2") {
+//     c++;
+//     r++;
+//   } else if (qq1 == "3") {
+//     c++;
+//     r++;
+//     j++;
+//     p++;
+//   }
+// // question 2 logic
+//   if (qq2 == "1") {
+//     p++;
+//     r++;
+//   } else if (qq2 == "2") {
+//     c++;
+//     p++;
+//     j++;
+//   } else if (qq2 == "3") {
+//     c++;
+//   } else if (qq2 == "4") {
+//     j++;
+//     r++;
+//   }
+// // question 3 logic
+//   if (qq3 =="yes") {
+//     r++;
+//     p++;
+//   } else {
+//     c++;
+//     j++;
+//     p++;
+//   }
+// // question 4 logic Below
+//   if (qq4 == "a") {
+//     j++;
+//     r++;
+//   } else if (qq4 == "b") {
+//     c++;
+//     p++;
+//   } else if (qq4 == "c") {
+//     r++;
+//     j++;
+//     p++;
+//   } else if (qq4 == "d") {
+//     j++;
+//   }
+// };
 
 $(document).ready(function() {
-  $('#formOne').submit(function(event) {
-    event.preventDefault();
-    var name = $("#nme").val();
-    var qq1 = $('#q1').val();
-    var qq2 = $('#q2').val();
-    var qq3 = $('#q3').val();
-    var qq4 = $('#q4').val();
-    countAnswers(q1, q2, q3, q4);
-    var answersArr = [c, r, p, j];
-    answers.Arr.sort();
-    var topResult = answersArr[0];
-})
-
-  // code for next buttons
   $('button').click(function(){
 
     if(this.id =='begin'){
@@ -103,14 +89,54 @@ $(document).ready(function() {
       $('#why').hide();
       $('#result').fadeIn();
     }
-
-    if(this.id == 'next4') {
-      $('#result').slideDown();
-      $('#Rizzzultzc').fadeIn();
-    } else if(this.id == 'next4') {
-      $('#result').slideDown();
-      $('#Rizzzultzr').fadeIn();
-    }
   });
 
-});
+
+
+
+
+
+// $("#formOne").submit(function(event){
+//   event.preventDefault();
+//   var name =$("#nme").val();
+//
+//
+// $("#formTwo").submit(function(event){
+//     event.preventDefault();
+//    var qq1 = $("#q1").val();
+//
+// $("#formThree").submit(function(event){
+//     event.preventDefault();
+//    var qq2 = $("#q2").val();
+//
+//   $("#formFour").submit(function(event){
+//     event.preventDefault();
+//    var qq3 = $("#q3").val();
+//
+//   $("#formFive").submit(function(event){
+//     event.preventDefault();
+//    var qq4 = $("#q4").val();
+//
+//   $('#formSix').submit(function(event){
+//     if(this.id == 'next4' && topResult === c) {
+//       $("#Rizzzultzc").show();
+//       $('#result').hide();
+//     } else if (this.id =="next4" && topResult === p) {
+//       $("#result").hide();
+//       $("#Rizzzultzc").show();
+//     } else if (this.id == 'next4' && topResult === j) {
+//       $("#Rizzzultzj").show();
+//       $('#result').hide();
+//     } else if(this.id == 'next4' && topResult === r) {
+//       $("#Rizzzultzr").show();
+//       $('#result').hide();
+//     }
+//
+//   });
+//
+// })
+//
+//   countAnswers(qq1, qq2, qq3, qq4);
+//   var answersArr = [c, r, p, j];
+//   answersArr.sort();
+//   var topResult = answersArr[0];
