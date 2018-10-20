52b165ee69680e5ef26ffcbf7d13b118a98e58ba
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-20 15:45:05 -0700

    more java and html tweaks to make form submit. none worked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,80 @@
-// var c=0;
-// var r=0;
-// var p=0;
-// var j=0;
-//
-//
-// function countAnswers(qq1, qq2, qq3, qq4,){
-// // question 1 logic
-//   if (qq1 == "1") {
-//     j++;
-//     p++;
-//   } else if (qq1 == "2") {
-//     c++;
-//     r++;
-//   } else if (qq1 == "3") {
-//     c++;
-//     r++;
-//     j++;
-//     p++;
-//   }
-// // question 2 logic
-//   if (qq2 == "1") {
-//     p++;
-//     r++;
-//   } else if (qq2 == "2") {
-//     c++;
-//     p++;
-//     j++;
-//   } else if (qq2 == "3") {
-//     c++;
-//   } else if (qq2 == "4") {
-//     j++;
-//     r++;
-//   }
-// // question 3 logic
-//   if (qq3 =="yes") {
-//     r++;
-//     p++;
-//   } else {
-//     c++;
-//     j++;
-//     p++;
-//   }
-// // question 4 logic Below
-//   if (qq4 == "a") {
-//     j++;
-//     r++;
-//   } else if (qq4 == "b") {
-//     c++;
-//     p++;
-//   } else if (qq4 == "c") {
-//     r++;
-//     j++;
-//     p++;
-//   } else if (qq4 == "d") {
-//     j++;
-//   }
-// };
+var c=0;
+var r=0;
+var p=0;
+var j=0;
 
+
+function countAnswers(qq1, qq2, qq3, qq4,){
+  // question 1 logic
+  if (qq1 == "1") {
+    j++;
+    p++;
+  } else if (qq1 == "2") {
+    c++;
+    r++;
+  } else if (qq1 == "3") {
+    c++;
+    r++;
+    j++;
+    p++;
+  };
+  // question 2 logic
+  if (qq2 == "1") {
+    p++;
+    r++;
+  } else if (qq2 == "2") {
+    c++;
+    p++;
+    j++;
+  } else if (qq2 == "3") {
+    c++;
+  } else if (qq2 == "4") {
+    j++;
+    r++;
+  };
+  // question 3 logic
+  if (qq3 =="yes") {
+    r++;
+    p++;
+  } else {
+    c++;
+    j++;
+    p++;
+  };
+  // question 4 logic Below
+  if (qq4 == "a") {
+    j++;
+    r++;
+  } else if (qq4 == "b") {
+    c++;
+    p++;
+  } else if (qq4 == "c") {
+    r++;
+    j++;
+    p++;
+  } else if (qq4 == "d") {
+    j++;
+  }
+
+};
 $(document).ready(function() {
-  $('button').click(function(){
 
+  console.log(name);
+
+  $("#formOne").submit(function(event){
+    var name =$("#nme").val();
+    // var qq1 =$('#q1').val();
+    // var qq2 =$('#q2').val();
+    // var qq3 =$('#q3').val();
+    // var qq4 =$('#q4').val();
+    // countAnswers(qq1,qq2,qq3,qq4);
+    // var answersARR = [c, r, p, j];
+    // answersARR.sort();
+    // var topResult = answersARR[0];
+  });
+
+  $('button').click(function(){
+    event.preventDefault();
     if(this.id =='begin'){
       $('#greeting').hide();
       $('#ID').show();
@@ -89,54 +104,19 @@ $(document).ready(function() {
       $('#why').hide();
       $('#result').fadeIn();
     }
+
+    // if (this.id == 'next4' && topResult === c) {
+    //   $('#result').hide();
+    //   $('#Rizzzultzc').show();
+    // } else if (this.id == 'next4' && topResult === p) {
+    //   $('#result').hide();
+    //   $('#Rizzzultzp').show();
+    // } else if (this.id == 'next4' && topResult === j) {
+    //   $('#result').hide();
+    //   $('#Rizzzultzj').show();
+    // } else if (this.id == 'next4' && topResult === r) {
+    //   $('#result').hide();
+    //   $('#Rizzzultzr').show();
+    // };
   });
-
-
-
-
-
-
-// $("#formOne").submit(function(event){
-//   event.preventDefault();
-//   var name =$("#nme").val();
-//
-//
-// $("#formTwo").submit(function(event){
-//     event.preventDefault();
-//    var qq1 = $("#q1").val();
-//
-// $("#formThree").submit(function(event){
-//     event.preventDefault();
-//    var qq2 = $("#q2").val();
-//
-//   $("#formFour").submit(function(event){
-//     event.preventDefault();
-//    var qq3 = $("#q3").val();
-//
-//   $("#formFive").submit(function(event){
-//     event.preventDefault();
-//    var qq4 = $("#q4").val();
-//
-//   $('#formSix').submit(function(event){
-//     if(this.id == 'next4' && topResult === c) {
-//       $("#Rizzzultzc").show();
-//       $('#result').hide();
-//     } else if (this.id =="next4" && topResult === p) {
-//       $("#result").hide();
-//       $("#Rizzzultzc").show();
-//     } else if (this.id == 'next4' && topResult === j) {
-//       $("#Rizzzultzj").show();
-//       $('#result').hide();
-//     } else if(this.id == 'next4' && topResult === r) {
-//       $("#Rizzzultzr").show();
-//       $('#result').hide();
-//     }
-//
-//   });
-//
-// })
-//
-//   countAnswers(qq1, qq2, qq3, qq4);
-//   var answersArr = [c, r, p, j];
-//   answersArr.sort();
-//   var topResult = answersArr[0];
+});
