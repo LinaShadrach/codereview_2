f2a558bfcf1e8223a84c780c6a60d0ecb1fd01cb
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-20 20:44:24 -0700

    add javascript for submiting forms from the java branch

--- js/scripts.js
+++ js/scripts.js
@@ -72,51 +72,63 @@ $(document).ready(function() {
     // var answersARR = [c, r, p, j];
     // answersARR.sort();
     // var topResult = answersARR[0];
+    
+      $('button').click(function(){
 
-  $('button').click(function(){
-    event.preventDefault();
-    if(this.id =='begin'){
-      $('#greeting').hide();
-      $('#ID').show();
-    }
+        if(this.id =='begin'){
+          $('#greeting').hide();
+          $('#ID').show();
+        }
 
-    if(this.id == 'continue'){
-      $('#ID').hide();
-      $('#brain').show();
-    }
+        if(this.id == 'continue'){
+          $('#ID').hide();
+          $('#brain').show();
+        }
 
-    if(this.id == 'go_on'){
-      $('#brain').hide();
-      $('#work').show();
-    }
+        if(this.id == 'go_on'){
+          $('#brain').hide();
+          $('#work').show();
+        }
 
-    if(this.id == 'next1'){
-      $("#work").hide();
-      $("#prsntpe").show();
-    }
+        if(this.id == 'next1'){
+          $("#work").hide();
+          $("#prsntpe").show();
+        }
 
-    if(this.id == 'next2'){
-      $('#prsntpe').hide();
-      $('#why').show();
-    }
+        if(this.id == 'next2'){
+          $('#prsntpe').hide();
+          $('#why').show();
+        }
 
-    if(this.id == 'next3') {
-      $('#why').hide();
-      $('#result').fadeIn();
-    }
+        if(this.id == 'next3') {
+          $('#why').hide();
+          $('#result').fadeIn();
+        }
+      });
+      $("#formOne").submit(function(event){
+        event.preventDefault();
+        var name =$("#nme").val();
+        console.log(name);
+      });
+      $("#formTwo").submit(function(event){
+        event.preventDefault();
+        var qq1 =$("#q1").val();
+        console.log(qq1);
+      });
+      $("#formThree").submit(function(event){
+        event.preventDefault();
+        var qq2 =$("#q2").val();
+        console.log(qq2);
+      });
+      $("#formFour").submit(function(event){
+        event.preventDefault();
+        var qq3 =$("#q3").val();
+        console.log(qq3);
+      });
+      $("#formFive").submit(function(event){
+        event.preventDefault();
+        var qq4 =$("#q4").val();
+        console.log(qq4);
+      });
 
-    // if (this.id == 'next4' && topResult === c) {
-    //   $('#result').hide();
-    //   $('#Rizzzultzc').show();
-    // } else if (this.id == 'next4' && topResult === p) {
-    //   $('#result').hide();
-    //   $('#Rizzzultzp').show();
-    // } else if (this.id == 'next4' && topResult === j) {
-    //   $('#result').hide();
-    //   $('#Rizzzultzj').show();
-    // } else if (this.id == 'next4' && topResult === r) {
-    //   $('#result').hide();
-    //   $('#Rizzzultzr').show();
-    // };
-  });
-});
+    });
